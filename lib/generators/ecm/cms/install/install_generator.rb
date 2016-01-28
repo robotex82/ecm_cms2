53545fd606710ac0149289a7010f010eb01635b7
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-01-28 21:06:12 +0100

    Fixed initializer name

--- lib/generators/ecm/cms/install/install_generator.rb
+++ lib/generators/ecm/cms/install/install_generator.rb
@@ -7,7 +7,7 @@ module Ecm
         source_root File.expand_path('../templates', __FILE__)
 
         def generate_intializer
-          copy_file 'ecm_cms.rb', 'config/initializers/ecm_cms.rb'
+          copy_file 'initializer.rb', 'config/initializers/ecm_cms.rb'
         end
       end
     end
