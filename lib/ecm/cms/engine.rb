d1e648a9203c9d68d6254f68f59c7452a6f9edde
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-10-08 02:27:18 +0200

    isolate engine

--- lib/ecm/cms/engine.rb
+++ lib/ecm/cms/engine.rb
@@ -1,6 +1,8 @@
 module Ecm
   module Cms
     class Engine < ::Rails::Engine
+      isolate_namespace Ecm::Cms
+      
       config.app_generators do |c|
         c.test_framework :rspec, fixture: true,
                                  fixture_replacement: nil
