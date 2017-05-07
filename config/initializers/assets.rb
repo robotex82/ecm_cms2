28fef760b7b348fe5987c9f11fdc3ed48f9ab80c
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-05-07 17:09:54 +0200

    Remove active admin references. Move asset statements to an initializer.

---
+++ config/initializers/assets.rb
@@ -0,0 +1,2 @@
+Rails.application.config.assets.precompile << 'ecm_cms.js'
+Rails.application.config.assets.precompile << 'ecm_cms.css'
--- lib/ecm/cms/engine.rb
+++ lib/ecm/cms/engine.rb
@@ -10,19 +10,6 @@ module Ecm
         c.integration_tool :rspec
         c.performance_tool :rspec
       end
-
-      initializer :ecm_cms_engine do
-        ::ActiveAdmin.setup do |config|
-          config.load_paths += Dir[root.join('app', 'admin')]
-          config.register_stylesheet 'ecm_cms.css'
-          config.register_javascript 'ecm_cms.js'
-        end
-      end if Gem::Specification.find_all_by_name('activeadmin').any?
-
-      initializer 'ecm_cms.asset_pipeline' do |app|
-        app.config.assets.precompile << 'ecm_cms.js'
-        app.config.assets.precompile << 'ecm_cms.css'
-      end
     end
   end
 end
