3346ca697f3b2f4ab4ec872decca7f7fcf1a6b6d
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-11 03:55:45 +0100

    Add example to Ecm::CmsHelper#cms_render_navigation. Add selected_class option to Ecm::CmsHelper#cms_render_navigation. Add item_html option to Ecm::CmsHelper#cms_render_navigation. Add link_html option to Ecm::CmsHelper#cms_render_navigation.

--- app/helpers/ecm/cms_helper.rb
+++ app/helpers/ecm/cms_helper.rb
@@ -1,11 +1,32 @@
 module Ecm::CmsHelper
+  # Example:
+  #
+  #     # This will render a bootstrap 4 compatible navigation
+  #     = cms_render_navigation(
+  #       :main,
+  #       renderer: :list,
+  #       container_css_class: 'navbar-nav',
+  #       selected_class: 'active',
+  #       item_html: { class: 'nav-item' },
+  #       link_html: { class: 'nav-link' }
+  #      )
   def cms_render_navigation(name, options = {})
-    options.reverse_merge! renderer: :bootstrap, expand_all: true, level: 1
+    options.reverse_merge!(
+      renderer:       :bootstrap,
+      expand_all:     true,
+      level:          1,
+      selected_class: nil,
+      item_html:      {},
+      link_html:      {}
+    )
 
     level               = options.delete(:level)
     expand_all          = options.delete(:expand_all)
     container_css_class = options.delete(:container_css_class)
     renderer            = options.delete(:renderer)
+    selected_class      = options.delete(:selected_class)
+    item_html           = options.delete(:item_html)
+    link_html           = options.delete(:link_html)
 
     level_as_array = (level).is_a?(Range) ? level.to_a : [level]
 
@@ -24,22 +45,24 @@ module Ecm::CmsHelper
 
     render_navigation(level: level, expand_all: expand_all, renderer: renderer) do |navigation|
       navigation.dom_class = container_css_class
+      navigation.selected_class = selected_class unless selected_class.nil?
       roots.each do |navigation_item|
-        build_navigation_item(navigation, navigation_item, container_css_class)
+        build_navigation_item(navigation, navigation_item, container_css_class, item_html, link_html)
       end
     end
   end
 
-  def build_navigation_item(navigation, item, container_css_class)
-    options = {}
-    options[:highlights_on] = /#{item.highlights_on}/ if item.highlights_on.present?
+  def build_navigation_item(navigation, item, container_css_class, item_html = {}, link_html = {})
     options = item.li_attributes.marshal_dump.delete_if { |_key, value| value.blank? }
+    options[:highlights_on] = /#{item.highlights_on}/ if item.highlights_on.present?
+
+    options.reverse_merge!(html: item_html.dup, link_html: link_html)
 
     navigation.dom_class = container_css_class
     if item.children.present?
-      navigation.item item.key, item.name, item.url, options do |sub_navigation|
+      navigation.item(item.key, item.name, item.url, options) do |sub_navigation|
         item.children.each do |sub_item|
-          build_navigation_item(sub_navigation, sub_item, container_css_class)
+          build_navigation_item(sub_navigation, sub_item, container_css_class, item_html, link_html)
         end
       end
     else
